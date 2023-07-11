48f82a7477a1e03b52d07e07750fa739caeb5d6e
ridhozahranp <ridhozahranp@students.amikom.ac.id>
2023-07-11 11:21:29 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace person
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Rahmat Fauzi Abu Bakar", 19);
+            person1.InfoPerson();
+
+            Console.WriteLine("================================");
+
+            Karyawan karyawan1 = new Karyawan("BH08234", "Sekertaris", "Fauzi", 19);
+            karyawan1.InfoKaryawan("BH08234", "Sekertaris");
+            karyawan1.InfoPerson();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace person
+{
+    public class Person
+    {
+        //field
+        string name;
+        int age;
+
+        //constructor
+        public Person(string n, int a)
+        {
+            this.name = n;
+            this.age = a;
+        }
+
+        //properties
+        public string Name;
+
+
+        public int Age;
+
+
+        //fungsi/method
+        public void InfoPerson()
+        {
+            Console.WriteLine("Nama saya {0} dan umur saya {1} tahun", name, age);
+        }
+    }
+
+    //class turunan
+    public class Karyawan : Person
+    {
+
+        //properties
+        public string karyawanid;
+        public string subject;
+
+        //constructor
+        public Karyawan(string kid, string sbj, string n, int a) : base(n, a)
+        {
+            karyawanid = kid;
+            subject = sbj;
+
+        }
+
+        //ovefride
+        public void InfoKaryawan(string kid, string sbj)
+        {
+
+            Console.WriteLine("Dengan ID {0} dan saya sebagai {1}", kid, sbj);
+        }
+
+        public void InfoPerson(string name, int age)
+        {
+
+            Console.WriteLine("Nama saya {0} dan umur saya {1} tahun", name, age);
+        }
+
+    }
+}
